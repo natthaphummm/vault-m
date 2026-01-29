0350fd4780a00e635af20276380789c8794b54ca
Natthaphum Phusong <info.hzelnat@gmail.com>
2026-01-29 21:38:44 +0700

    feat: add home page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,25 @@
-import Versions from './components/Versions'
+// import Versions from './components/Versions'
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@renderer/components/ui/tabs'
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
-      <Tabs defaultValue="account" className="w-[400px]">
+    <main className="min-h-screen font-sans mx-auto p-2 bg-amber-200">
+      <Tabs defaultValue="inventory" className="w-full">
         <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+          <TabsTrigger value="inventory">Inventory</TabsTrigger>
+          <TabsTrigger value="crafting">Crafting</TabsTrigger>
         </TabsList>
-        <TabsContent value="account">Make changes to your account here.</TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
+        <TabsContent value="inventory" className="bg-amber-600">
+          Inventory
+        </TabsContent>
+        <TabsContent value="crafting" className="bg-amber-600">
+          Crafting
+        </TabsContent>
       </Tabs>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    </main>
   )
 }
 
