91c57e97df2140c56fe74201d8eeac3c94efffae
Natthaphum Phusong <info.hzelnat@gmail.com>
2026-01-29 21:02:36 +0700

    feat: add shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,20 @@
 import Versions from './components/Versions'
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@renderer/components/ui/tabs'
+
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">Make changes to your account here.</TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="actions">
         <div className="action">
